e23c39f5e8f41ffa84c6afb4335471056abfd775
MAGS <mags@mags.dk>
2025-02-13 17:35:09 +0100

    Remove RabbitMQ Python example scripts and update Docker Compose configuration

--- MessageBroker/RabbitMQ/Python/amqp/send-amqp.py
+++ MessageBroker/RabbitMQ/Python/amqp/send-amqp.py
@@ -4,7 +4,7 @@ import sys
 # Forbindelsesindstillinger
 credentials = pika.PlainCredentials('admin', 'admin')  
 parameters = pika.ConnectionParameters(
-    host='localhost',
+    host='10.135.61.102',
     port=5672,
     credentials=credentials,
     heartbeat=600
--- MessageBroker/RabbitMQ/Python/mqtt/receive-mqtt.py
+++ MessageBroker/RabbitMQ/Python/mqtt/receive-mqtt.py
@@ -4,19 +4,20 @@ import paho.mqtt.client as mqtt
 def on_message(client, userdata, message):
     print(f"Modtaget besked: {message.payload.decode()} på emne: {message.topic}")
 
-# Callback-funktion til håndtering af forbindelsesfejl
 def on_connect(client, userdata, flags, rc):
     print(f"Forbundet med resultatkode: {rc}")
     client.subscribe(topic)  # Abonner på emnet, når forbindelsen er etableret
 
 # MQTT konfiguration
 broker = "localhost"  # eller IP-adressen til din RabbitMQ-server
-port = 1884  # Porten, du har konfigureret til MQTT
-topic = "test/topic"
+port = 1883  # Porten, du har konfigureret til MQTT
+topic = "test"
 
 # Opret en MQTT-klient
 client = mqtt.Client()
 
+# Sæt brugernavn og adgangskode
+client.username_pw_set("admin", "admin")  
 # Tilmeld callback-funktioner
 client.on_connect = on_connect
 client.on_message = on_message
